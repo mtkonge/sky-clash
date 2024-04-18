e5ccfa0b38f21b92c581672088b7a5757dd4e302
Mikkel Kongsted <mtkongsted@gmail.com>
2024-04-18 14:47:02 +0200

    internet access for board

--- board/board.ino
+++ board/board.ino
@@ -1,8 +1,60 @@
+#include <WiFiNINA.h>
+#include "env.h"
+
+char hostName[] = "www.google.com";
+int status = WL_IDLE_STATUS;
+int pingResult;
 
 void setup() {
+
+  Serial.begin(9600);
+  // attempt to connect to Wifi network:
+
+  // Wifi ssid and pass needs to be defined in env.h
+
+  while (status != WL_CONNECTED) {
+
+    Serial.print("Attempting to connect to network: ");
+
+    Serial.println(ssid);
+
+    status = WiFi.begin(ssid, pass);
+
+
+    delay(10000);
+
+  }
+
+  Serial.print("You're connected to the network: ");
+  Serial.println(ssid);
+
 
 }
 
 void loop() {
+  Serial.print("Pinging ");
+
+  Serial.println(hostName);
+
+
+  pingResult = WiFi.ping(hostName);
+
+  if (pingResult >= 0) {
+
+    Serial.print("SUCCESS! RTT = ");
+
+    Serial.print(pingResult);
+
+    Serial.println(" ms");
+
+  } else {
+
+    Serial.print("FAILED! Error code: ");
+
+    Serial.println(pingResult);
+
+  }
+
+  delay(5000);
 
 }
