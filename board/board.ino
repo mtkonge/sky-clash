a4798d4cb456e765b7b3ef6bdece504fc851eb98
SimonFJ20 <simonfromjakobsen@gmail.com>
2024-04-23 12:06:15 +0200

    reading rfid from scanner

--- board/board.ino
+++ board/board.ino
@@ -1,7 +1,9 @@
 #include <Adafruit_PN532.h>
 
+#define IRQ_PIN 1
+#define RSTO_PIN 0
 
-Adafruit_PN532 rfid(0, 1, &Wire);
+Adafruit_PN532 rfid(IRQ_PIN, RSTO_PIN, &Wire);
 
 void setup() {
   Serial.begin(9600);
@@ -18,9 +20,22 @@ void setup() {
   }
 
   uint32_t version = rfid.getFirmwareVersion();
+  while (version == 0) {
+    Serial.println("version == 0");
+    delay(500);
+    bool success = rfid.begin();
+    if (!success) {
+      Serial.println("RFID failed");
+    }
+    version = rfid.getFirmwareVersion();
+  }
   Serial.println(String("version = ") + version);
+
+  // rfid.setPassiveActivationRetries(0xFF);
 }
- 
+
+int slurs = 0;
+
 void loop() {
   uint8_t success;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
@@ -28,11 +43,19 @@ void loop() {
 
   success = rfid.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
 
+  Serial.println(String("slurs = ") + slurs);
+  slurs += 1;
+
   if (success) {
     Serial.println("Found an ISO14443A card");
-    Serial.print("  UID Length: ");
-    Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print(String("  UID Length: ") + uidLength + " bytes");
     Serial.print("  UID Value: ");
+    for (uint8_t i=0; i < uidLength; i++) {
+      Serial.print(" 0x");
+      Serial.print(uid[i], HEX);
+    }
+    Serial.println("");
+    delay(100);
   } else {
     Serial.println("Failed");
     delay(500);
