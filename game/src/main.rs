c84a731c32056ad2e4ec2c5a5e9ed810ceaf4c6f
Mikkel Kongsted <mtkongsted@gmail.com>
2024-04-23 10:51:59 +0200

    remove ui stuff

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,17 +1,11 @@
 #![allow(dead_code)]
 
-use main_menu::MainMenu;
-
 mod engine;
-mod hero_creator;
-mod main_menu;
-mod ui;
 
 fn main() {
     let mut game = engine::Game::new().unwrap();
 
     let mut ctx = game.context();
-    ctx.add_system(MainMenu);
 
     game.run();
 }
