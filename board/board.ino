85a43ffc1fb14dcd8a6871a2c7db3ecd4f87a527
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2024-04-23 11:05:16 +0200

    add arduino build stuff + rfid

--- board/board.ino
+++ board/board.ino
@@ -1,103 +1,40 @@
-#include <SPI.h>
-#include <WiFiNINA.h>
-#include "env.h"
+#include <Adafruit_PN532.h>
 
-int status = WL_IDLE_STATUS;
-int pingResult;
 
-WiFiClient client;
-
-IPAddress server(192,168,132,183);
-
+Adafruit_PN532 rfid(0, 1, &Wire);
 
 void setup() {
   Serial.begin(9600);
 
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to network: ");
-    Serial.println(ssid);
-    status = WiFi.begin(ssid, pass);
-    delay(10000);
+  while (!Serial) {
+    delay(100);
   }
-  Serial.print("You're connected to the network: ");
-  Serial.println(ssid);
 
-  printWifiStatus();
+  Serial.println("Start");
 
-  ping("192.168.132.183");
-
-  post_request();
-}
-
-void ping(char* ip) {
-  Serial.print("Pinging ");
-  Serial.println(ip);
-  pingResult = WiFi.ping(ip);
-
-  if (pingResult >= 0) {
-    Serial.print("SUCCESS! RTT = ");
-    Serial.print(pingResult);
-    Serial.println(" ms");
-  } else {
-    Serial.print("FAILED! Error code: ");
-    Serial.println(pingResult);
+  bool success = rfid.begin();
+  if (!success) {
+    Serial.println("RFID failed");
   }
+
+  uint32_t version = rfid.getFirmwareVersion();
+  Serial.println(String("version = ") + version);
 }
-
-
-
-void printWifiStatus() {
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
-
-}
-void get_google_html() {
-  Serial.println("\nStarting connection to server...");
-  if (client.connect("www.google.com", 80)) {
-    Serial.println("connected to server");
-    client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host: www.google.com");
-    client.println("Connection: close");
-    client.println();
-  }
-}
-
-
-void post_request() {
-  String data = "{\"rfid\": \"aiojfejiofeq\", \"hero_type\": 4}";
-  Serial.println("WE ARE MAKING A POST REQUEST NOBODY BREATH");
-  if (client.connect("192.168.132.183", 8080)) {
-    Serial.println("WE ARE IN !!!!");
-    client.println("POST /create_hero HTTP/1.1");
-    client.println("Host: 192.168.132.183:8080");
-    client.println("Content-Type: application/json");
-    client.print("Content-Length: ");
-    client.println(data.length());
-    client.println();
-    client.print(data);
-  } else {
-    Serial.println("Could not connect");
-  }
-}
-
-
+ 
 void loop() {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
+  uint8_t uidLength;
 
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
+  success = rfid.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  if (success) {
+    Serial.println("Found an ISO14443A card");
+    Serial.print("  UID Length: ");
+    Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print("  UID Value: ");
+  } else {
+    Serial.println("Failed");
+    delay(500);
   }
-
-
-
-
 }
---
+++ board/wifi.ino
@@ -0,0 +1,78 @@
+#include "env.h"
+#include <SPI.h>
+#include <WiFiNINA.h>
+
+int status = WL_IDLE_STATUS;
+int pingResult;
+
+WiFiClient client;
+
+void connect_to_network() {
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to network: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(10000);
+  }
+}
+
+IPAddress server(192, 168, 132, 183);
+
+void ping(char* ip) {
+  Serial.print("Pinging ");
+  Serial.println(ip);
+  pingResult = WiFi.ping(ip);
+
+  if (pingResult >= 0) {
+    Serial.print("SUCCESS! RTT = ");
+    Serial.print(pingResult);
+    Serial.println(" ms");
+  } else {
+    Serial.print("FAILED! Error code: ");
+    Serial.println(pingResult);
+  }
+}
+
+void printWifiStatus() {
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+
+}
+void get_google_html() {
+  Serial.println("\nStarting connection to server...");
+  if (client.connect("www.google.com", 80)) {
+    Serial.println("connected to server");
+    client.println("GET /search?q=arduino HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
+    client.println();
+  }
+}
+
+
+void post_request() {
+  String data = "{\"rfid\": \"aiojfejiofeq\", \"hero_type\": 4}";
+  Serial.println("WE ARE MAKING A POST REQUEST NOBODY BREATH");
+  if (client.connect("192.168.132.183", 8080)) {
+    Serial.println("WE ARE IN !!!!");
+    client.println("POST /create_hero HTTP/1.1");
+    client.println("Host: 192.168.132.183:8080");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.println(data.length());
+    client.println();
+    client.print(data);
+  } else {
+    Serial.println("Could not connect");
+  }
+}
+
