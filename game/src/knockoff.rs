0288402e3b4731469d93fc74f7ac421d91cec649
Mikkel Kongsted <mtkongsted@gmail.com>
2024-05-15 11:42:50 +0200

    hero match stats

--- game/src/knockoff.rs
+++ game/src/knockoff.rs
@@ -1,6 +1,9 @@
 use engine::{query, rigid_body::RigidBody, spawn, Component, Context, Error, System};
 
-use crate::{hurtbox::MatchHero, player_movement::PlayerMovement};
+use crate::{
+    hurtbox::{HeroKind, MatchHero},
+    player_movement::PlayerMovement,
+};
 
 fn player_died_text(loser: &shared::HeroKind, winner: &shared::HeroKind, counter: f64) -> String {
     let counter = counter as u8 % 3;
@@ -30,9 +33,102 @@ fn win_condition(ctx: &mut Context, loser_id: engine::Id) {
     let trash_talk = player_died_text(loser, &winner, trash_talk_offset);
     let font = ctx.load_font("textures/ttf/OpenSans.ttf", 48).unwrap();
     let text = ctx.render_text(font, &trash_talk, (255, 255, 255)).unwrap();
-    let (text_width, _) = ctx.text_size(font, &trash_talk).unwrap();
-    ctx.draw_texture(text.texture, (1280 - text_width as i32) / 2, 100)
+    ctx.draw_texture(text.texture, (1280 - text.size.0) / 2, 100)
         .unwrap();
+}
+
+fn draw_match_stats_background(
+    ctx: &mut Context,
+    border_color: (u8, u8, u8),
+    border_thickness: i32,
+    pos: (i32, i32),
+    offset: (i32, i32),
+    text_size: (i32, i32),
+    stats_size: (u32, u32),
+) {
+    ctx.draw_rect(
+        border_color,
+        pos.0 + offset.0 - border_thickness,
+        pos.1 + stats_size.0 as i32 / 2 - border_thickness + offset.1 - border_thickness,
+        text_size.0 as u32 + border_thickness as u32 * 2,
+        text_size.1 as u32 + border_thickness as u32 * 2,
+    )
+    .unwrap();
+    ctx.draw_rect(border_color, pos.0, pos.1, stats_size.0, stats_size.1)
+        .unwrap();
+
+    ctx.draw_rect(
+        (0, 0, 0),
+        pos.0 + border_thickness,
+        pos.1 + border_thickness,
+        stats_size.0 - border_thickness as u32 * 2,
+        stats_size.1 - border_thickness as u32 * 2,
+    )
+    .unwrap();
+    ctx.draw_rect(
+        (0, 0, 0),
+        pos.0 + offset.0,
+        pos.1 + stats_size.0 as i32 / 2 - border_thickness + offset.1,
+        text_size.0 as u32,
+        text_size.1 as u32,
+    )
+    .unwrap();
+}
+
+fn draw_match_stats(ctx: &mut Context, match_hero: MatchHero) {
+    let lives = match_hero.lives.to_string();
+    let font = ctx.load_font("textures/ttf/OpenSans.ttf", 24).unwrap();
+    let text = ctx.render_text(font, lives, (255, 255, 255)).unwrap();
+    let stats_size = (50, 50);
+    let border_thickness = 2;
+    let border_color = (255, 255, 255);
+
+    let hero_sprite = {
+        let path = crate::hero_info::HeroInfo::from(match_hero.hero.hero_type).texture_path;
+        ctx.load_texture(path).unwrap()
+    };
+
+    let (pos, offset) = match match_hero.kind {
+        HeroKind::Hero1 => (
+            (0, 0),
+            (
+                stats_size.0 as i32 - stats_size.0 as i32 / 5,
+                stats_size.0 as i32 / 8,
+            ),
+        ),
+        HeroKind::Hero2 => (
+            (1280 - stats_size.0 as i32, 0),
+            (
+                -text.size.0 + stats_size.0 as i32 / 5,
+                stats_size.0 as i32 / 8,
+            ),
+        ),
+    };
+
+    draw_match_stats_background(
+        ctx,
+        border_color,
+        border_thickness,
+        pos,
+        offset,
+        text.size,
+        stats_size,
+    );
+
+    ctx.draw_texture_sized(
+        hero_sprite,
+        pos.0 + border_thickness,
+        pos.1 + border_thickness,
+        stats_size.0 - border_thickness as u32 * 2,
+        stats_size.1 - border_thickness as u32 * 2,
+    )
+    .unwrap();
+    ctx.draw_texture(
+        text.texture,
+        pos.0 + offset.0,
+        pos.1 + stats_size.0 as i32 / 2 - border_thickness + offset.1,
+    )
+    .unwrap();
 }
 
 pub struct KnockoffSystem(pub u64);
@@ -43,13 +139,16 @@ impl System for KnockoffSystem {
     }
     fn on_update(&self, ctx: &mut Context, delta: f64) -> Result<(), Error> {
         let max_offset_from_screen = 200.0;
-        for loser_id in query!(ctx, PlayerMovement, RigidBody, MatchHero).clone() {
-            let rigid_body = ctx.select::<RigidBody>(loser_id).clone();
+        for id in query!(ctx, PlayerMovement, RigidBody, MatchHero).clone() {
+            let rigid_body = ctx.select::<RigidBody>(id).clone();
+            let match_hero = ctx.select::<MatchHero>(id).clone();
+            draw_match_stats(ctx, match_hero);
             if rigid_body.pos.0 + rigid_body.rect.0 < -max_offset_from_screen
                 || rigid_body.pos.0 > 1280.0 + max_offset_from_screen
                 || rigid_body.pos.1 + rigid_body.rect.1 < -max_offset_from_screen
                 || rigid_body.pos.1 > 720.0 + max_offset_from_screen
             {
+                let loser_id = id;
                 let stats = ctx.select::<MatchHero>(loser_id);
                 if stats.lives > 0 {
                     stats.lives -= 1;
--- game/src/player_movement.rs
+++ game/src/player_movement.rs
@@ -37,22 +37,22 @@ impl System for PlayerMovementSystem {
     fn on_update(&self, ctx: &mut engine::Context, delta: f64) -> Result<(), engine::Error> {
         for id in query!(ctx, PlayerMovement, RigidBody, Collider) {
             let key_set = ctx.select::<PlayerMovement>(id).clone().key_set;
-            let d_down = ctx.key_pressed(key_set.right());
-            let a_down = ctx.key_pressed(key_set.left());
-            let w_down = ctx.key_pressed(key_set.up());
+            let right_pressed = ctx.key_pressed(key_set.right());
+            let left_pressed = ctx.key_pressed(key_set.left());
+            let up_pressed = ctx.key_pressed(key_set.up());
             let collider = ctx.select::<Collider>(id).clone();
             let body = ctx.select::<RigidBody>(id);
 
-            if d_down && !a_down && body.vel.0 < 400.0 {
+            if right_pressed && !left_pressed && body.vel.0 < 400.0 {
                 body.vel.0 += 400.0 * delta * 8.0
-            } else if a_down && !d_down && body.vel.0 > (-400.0) {
+            } else if left_pressed && !right_pressed && body.vel.0 > (-400.0) {
                 body.vel.0 -= 400.0 * delta * 8.0
             }
 
             if collider
                 .colliding
                 .is_some_and(|dir| dir.facing(engine::collision::Direction::Bottom))
-                && w_down
+                && up_pressed
             {
                 body.vel.1 = -800.0;
             }
