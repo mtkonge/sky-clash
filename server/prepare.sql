ed39b1a15901a12f726fa088d37d7f7a37b6ae78
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2024-05-21 12:39:53 +0200

    server: prepare statement update

--- server/prepare.sql
+++ server/prepare.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS heroes (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   rfid TEXT NOT NULL,
   level INTEGER NOT NULL,
-  hero_type INTEGER NOT NULL,
+  kind INTEGER NOT NULL,
   strength_points INTEGER NOT NULL,
   agility_points INTEGER NOT NULL,
   defence_points INTEGER NOT NULL
