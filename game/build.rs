6cd65492a81f023b6ca54384d4c6ea81e323c288
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2024-06-04 09:10:41 +0200

    fix cwinge dead code warning

--- game/build.rs
+++ game/build.rs
@@ -1,8 +1,6 @@
-fn windows_msvc() {
-    println!(r"cargo:rustc-link-search=vendored-deps/msvc/libs");
-}
-
 fn main() {
-    #[cfg(all(target_os = "windows", target_env="msvc"))]
-    windows_msvc();
+    #[cfg(all(target_os = "windows", target_env = "msvc"))]
+    {
+        println!(r"cargo:rustc-link-search=vendored-deps/msvc/libs");
+    }
 }
