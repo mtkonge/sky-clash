c9c7a355d1df5f905bf678c1e18b0a1bdb93281c
SimonFJ20 <simonfromjakobsen@gmail.com>
2024-06-04 10:15:51 +0200

    move v2 into physics

--- game/engine/src/lib.rs
+++ game/engine/src/lib.rs
@@ -11,7 +11,6 @@ mod query_runner;
 mod system;
 mod text;
 mod texture;
-mod v2;
 
 pub mod collision;
 pub mod physics;
@@ -22,8 +21,8 @@ pub mod ui;
 pub use self::{
     collision::CollisionSystem, collision::SolidCollider, component::Component,
     context::ComponentQuery, context::Context, context::DrawTextureOpts, error::Error, game::Game,
-    id::Id, query_runner::QueryRunner, shared_ptr::SharedPtr, system::System, text::Text,
-    texture::Texture, v2::clamp, v2::max, v2::min, v2::V2,
+    id::Id, physics::clamp, physics::max, physics::min, physics::V2, query_runner::QueryRunner,
+    shared_ptr::SharedPtr, system::System, text::Text, texture::Texture,
 };
 pub use component_macro::Component;
 pub use sdl2::controller::Button as ControllerButton;
